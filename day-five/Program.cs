88a39684f96a72b85a85ae81c076c08af0f2ec0e
DestroyerX7 <Bojera22@gmail.com>
2024-12-05 01:00:18 -0700

    made day five all one part

--- day-five/Program.cs
+++ day-five/Program.cs
@@ -10,14 +10,10 @@ public class Program
     private static void Main(string[] args)
     {
         string[] input = File.ReadAllLines("D:/VS Code Projects/advent-of-code-2024/day-five/input.txt");
-        PartOne(input);
-        PartTwo(input);
-    }
 
-    private static void PartOne(string[] input)
-    {
         Dictionary<int, HashSet<int>> rules = new();
         int middleSum = 0;
+        int middleSumUnordered = 0;
 
         foreach (string line in input)
         {
@@ -40,117 +36,64 @@ public class Program
             else if (line.Contains(','))
             {
                 int[] numsToAdd = line.Split(',').Select(int.Parse).ToArray();
-                HashSet<int> numsAdded = new();
-                bool inOrder = true;
-
-                foreach (int num in numsToAdd)
-                {
-                    if (rules.ContainsKey(num) && numsAdded.Any(n => rules[num].Contains(n)))
-                    {
-                        inOrder = false;
-                        break;
-                    }
-
-                    numsAdded.Add(num);
-                }
+                bool inOrder = IsOrdered(numsToAdd, rules);
 
                 if (inOrder)
                 {
                     middleSum += numsToAdd[numsToAdd.Length / 2];
                 }
+                else
+                {
+                    Reorder(numsToAdd, rules);
+                    middleSumUnordered += numsToAdd[numsToAdd.Length / 2];
+                }
             }
         }
 
         Console.WriteLine("Part One : " + middleSum);
+        Console.WriteLine("Part Two : " + middleSumUnordered);
     }
 
-    private static void PartTwo(string[] input)
+    private static void Reorder(int[] nums, Dictionary<int, HashSet<int>> rules)
     {
-        Dictionary<int, HashSet<int>> rules = new();
-        int middleSum = 0;
+        bool inOrder = false;
 
-        foreach (string line in input)
+        // Swaps numbers that are not in the right order one at a time until they are in order.
+        while (!inOrder)
         {
-            if (line.Contains('|'))
+            // Looks for a number that should be before any of the numbers in rules and then swaps them.
+            for (int i = 0; i < nums.Length; i++)
             {
-                int[] rule = line.Split('|').Select(int.Parse).ToArray();
-
-                if (rules.ContainsKey(rule[0]))
+                if (rules.ContainsKey(nums[i]))
                 {
-                    rules[rule[0]].Add(rule[1]);
-                }
-                else
-                {
-                    rules[rule[0]] = new()
+                    for (int j = 0; j < i; j++)
                     {
-                        rule[1]
-                    };
-                }
-            }
-            else if (line.Contains(','))
-            {
-                int[] numsToAdd = line.Split(',').Select(int.Parse).ToArray();
-                HashSet<int> numsAdded = new();
-                bool inOrder = true;
-
-                foreach (int num in numsToAdd)
-                {
-                    if (rules.ContainsKey(num) && numsAdded.Any(n => rules[num].Contains(n)))
-                    {
-                        inOrder = false;
-                        break;
-                    }
-
-                    numsAdded.Add(num);
-                }
-
-                if (!inOrder)
-                {
-                    Reorder(numsToAdd);
-                    middleSum += numsToAdd[numsToAdd.Length / 2];
-                }
-            }
-        }
-
-        Console.WriteLine("Part Two : " + middleSum);
-
-        void Reorder(int[] nums)
-        {
-            bool inOrder = false;
-
-            // Swaps numbers that are not in the right order one at a time until they are in order.
-            while (!inOrder)
-            {
-                // Looks for a number that should be before any of the numbers in rules and then swaps them.
-                for (int i = 0; i < nums.Length; i++)
-                {
-                    if (rules.ContainsKey(nums[i]))
-                    {
-                        for (int j = 0; j < i; j++)
+                        if (rules[nums[i]].Contains(nums[j]))
                         {
-                            if (rules[nums[i]].Contains(nums[j]))
-                            {
-                                (nums[i], nums[j]) = (nums[j], nums[i]);
-                                break;
-                            }
+                            (nums[i], nums[j]) = (nums[j], nums[i]);
+                            break;
                         }
                     }
                 }
-
-                // Checks if the nums are now in order after doing the previous step
-                HashSet<int> numsAdded = new();
-                inOrder = true;
-                foreach (int num in nums)
-                {
-                    if (rules.ContainsKey(num) && numsAdded.Any(n => rules[num].Contains(n)))
-                    {
-                        inOrder = false;
-                        break;
-                    }
-
-                    numsAdded.Add(num);
-                }
             }
+
+            inOrder = IsOrdered(nums, rules);
         }
+    }
+
+    private static bool IsOrdered(int[] nums, Dictionary<int, HashSet<int>> rules)
+    {
+        HashSet<int> numsAdded = new();
+        foreach (int num in nums)
+        {
+            if (rules.ContainsKey(num) && numsAdded.Any(n => rules[num].Contains(n)))
+            {
+                return false;
+            }
+
+            numsAdded.Add(num);
+        }
+
+        return true;
     }
 }
