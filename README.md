10f4dd2f309d5e2686feba45fe6e5a8aa628e007
DestroyerX7 <Bojera22@gmail.com>
2024-12-26 00:01:23 -0700

    did what I could of day twenty-five

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 New year, new AoC 😎
 
 Hopefully I can complete more days, and get more stars than last year 😁🤞.
+
+Doing the best I could after day 25 I got a total of 42 stars which I am very proud of. This is my 3rd year doing Aoc, my first year I got 24 stars, and last year I got 33. Day 16 was the only day where I didn't get any stars, and I think I could have solved it if I spent a little more time on it. I will still try to solve it within the next couple of days. Overall, it was a good year of Aoc 👍.
---
+++ day-twenty-five/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace day_twenty_five;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] input = File.ReadAllLines("D:/VS Code Projects/advent-of-code-2024/day-twenty-five/input.txt");
+
+        List<int[]> keyHeights = new();
+        List<int[]> lockHeights = new();
+
+        for (int i = 0; i < input.Length; i += 8)
+        {
+            bool isLock = input[i][0] == '#';
+
+            int[] heights = new int[5];
+
+            for (int j = 0; j < 5; j++)
+            {
+                int height = isLock ? 0 : 5;
+
+                if (isLock)
+                {
+                    for (int k = i + 1; input[k][j] == '#'; k++)
+                    {
+                        height++;
+                    }
+                }
+                else
+                {
+                    for (int k = i + 1; input[k][j] == '.'; k++)
+                    {
+                        height--;
+                    }
+                }
+
+                heights[j] = height;
+            }
+
+            if (isLock)
+            {
+                lockHeights.Add(heights);
+            }
+            else
+            {
+                keyHeights.Add(heights);
+            }
+        }
+
+        int numFits = 0;
+
+        foreach (int[] keyHeight in keyHeights)
+        {
+            foreach (int[] lockHeight in lockHeights)
+            {
+                bool fits = true;
+
+                for (int i = 0; i < 5; i++)
+                {
+                    if (keyHeight[i] + lockHeight[i] > 5)
+                    {
+                        fits = false;
+                        break;
+                    }
+                }
+
+                if (fits)
+                {
+                    numFits++;
+                }
+            }
+        }
+
+        Console.WriteLine("Part One : " + numFits);
+    }
+}
